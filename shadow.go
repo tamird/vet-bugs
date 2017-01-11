0e3aaea331f252225105599e2307b46d25e19329
Tamir Duberstein <tamird@gmail.com>
2017-01-11 14:49:59 -0500

    Demonstrate that defining over the loop variable is a false positive

--- shadow.go
+++ shadow.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"strings"
 )
 
@@ -11,13 +10,21 @@ func main() {
 	{
 		// go1.7.4, go1.8rc1: declaration of "ctx" shadows declaration at shadow.go:9
 		ctx, cancel := context.WithCancel(ctx)
+		_ = ctx
 		cancel()
 	}
-	fmt.Println(ctx)
 
 	var strs []string
 	for _, str := range strs {
-		// go1.8rc1: declaration of "str" shadows declaration at shadow.go:19
+		_ = str
+
+		// no warning
+		str := "foo"
+		_ = str
+	}
+	for _, str := range strs {
+		// go1.8rc1: declaration of "str" shadows declaration at shadow.go:25
 		str := strings.Replace(str, "1", "2", -1)
+		_ = str
 	}
 }
